7330b2ad1cdf5519b9751ae765584719f007ae6a
Victor <victor.gabrielnord@gmail.com>
2026-02-20 22:33:40 -0300

    componente card

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cards } from "./cards/cards";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cards],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Criando-Rotas');
+  protected readonly title = signal('Rotas');
 }
